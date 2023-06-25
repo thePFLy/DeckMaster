fa4d5b7e316cc4824c1b2b7cc96b68069c489e35
PFLY <HE202071@students.ephec.be>
2023-06-25 12:30:59 +0200

    classe

--- soundWindowsMVP.py
+++ soundWindowsMVP.py
@@ -1,78 +1,59 @@
-from __future__ import print_function
 from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 
-def listActiveSession():
-    # Get all executed audio sessions
-    sessions = AudioUtilities.GetAllSessions()
-    # Flag to track if a matching session is found
-    match_found = False
-    # Browse audio sessions
-    for session in sessions:
-        # Get the display name of the session
-        session_name = session.Process.name() if session.Process else "System Sounds"
-        # Check if the session name ends with ".exe"
-        if session_name.endswith(".exe"):
-            session_name = session_name[:-4]  # Remove the ".exe" extension
-        # Display the name of the session
-        print("Session Name: %s" % session_name)
-    
+class VolumeModifier:
+    def __init__(self):
+        self.sessions = AudioUtilities.GetAllSessions()
 
-def sessionSelection():
-    # Get all executed audio sessions
-    sessions = AudioUtilities.GetAllSessions()
-    
-    # Display the list of active sessions
-    for index, session in enumerate(sessions):
-        session_name = session.Process.name() if session.Process else "System Sounds"
-        if session_name.endswith(".exe"):
-            session_name = session_name[:-4]  # Remove the ".exe" extension
-        print("%d. %s" % (index + 1, session_name))
-    
-    # Prompt the user to select a session
-    while True:
-        selection = input("Select a session by entering its number: ")
-        try:
-            selection_index = int(selection) - 1
-            if 0 <= selection_index < len(sessions):
-                return sessions[selection_index]
-            else:
+    def list_active_sessions(self):
+        for session in self.sessions:
+            session_name = session.Process.name() if session.Process else "System Sounds"
+            if session_name.endswith(".exe"):
+                session_name = session_name[:-4]  # Remove the ".exe" extension
+            print("Session Name: %s" % session_name)
+
+    def select_session(self):
+        for index, session in enumerate(self.sessions):
+            session_name = session.Process.name() if session.Process else "System Sounds"
+            if session_name.endswith(".exe"):
+                session_name = session_name[:-4]  # Remove the ".exe" extension
+            print("%d. %s" % (index + 1, session_name))
+
+        while True:
+            selection = input("Select a session by entering its number: ")
+            try:
+                selection_index = int(selection) - 1
+                if 0 <= selection_index < len(self.sessions):
+                    return self.sessions[selection_index]
+                else:
+                    print("Invalid selection. Please enter a valid number.")
+            except ValueError:
                 print("Invalid selection. Please enter a valid number.")
-        except ValueError:
-            print("Invalid selection. Please enter a valid number.")
 
+    def modify_volume(self, session):
+        volume = session.SimpleAudioVolume
 
-def modifyVolume(session):
-    # Get the volume control of the session
-    volume = session.SimpleAudioVolume
-    
-    # Prompt the user to enter a new volume value
-    while True:
-        new_volume = input("Enter the new volume value (0.0 to 1.0): ")
-        try:
-            new_volume = float(new_volume)
-            if 0.0 <= new_volume <= 1.0:
-                break
-            else:
-                print("Invalid volume value. Please enter a value between 0.0 and 1.0.")
-        except ValueError:
-            print("Invalid volume value. Please enter a valid number.")
-    
-    # Set the new volume value
-    volume.SetMasterVolume(new_volume, None)
-    
-    # Display the updated volume value
-    updated_volume = volume.GetMasterVolume()
-    print("Volume updated to: %s" % updated_volume)
+        while True:
+            new_volume = input("Enter the new volume value (0.0 to 1.0): ")
+            try:
+                new_volume = float(new_volume)
+                if 0.0 <= new_volume <= 1.0:
+                    break
+                else:
+                    print("Invalid volume value. Please enter a value between 0.0 and 1.0.")
+            except ValueError:
+                print("Invalid volume value. Please enter a valid number.")
 
+        volume.SetMasterVolume(new_volume, None)
+        updated_volume = volume.GetMasterVolume()
+        print("Volume updated to: %s" % updated_volume)
 
+    def run(self):
+        self.list_active_sessions()
+        session = self.select_session()
+        self.modify_volume(session)
 
-
-def main():
-    listActiveSession()
-    session = sessionSelection()
-    modifyVolume(session)
 
 if __name__ == '__main__':
     parser = ArgumentParser(
@@ -85,4 +66,6 @@ if __name__ == '__main__':
     3. Change the volume\n
     ''')
     args = parser.parse_args()
-    main()
+
+    volume_modifier = VolumeModifier()
+    volume_modifier.run()
