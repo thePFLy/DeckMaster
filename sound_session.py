5e2207ee5410c3b14d480acce33350d38d7fbeaa
PFLY <HE202071@students.ephec.be>
2023-06-25 13:37:13 +0200

    PRE POST

--- sound_session.py
+++ sound_session.py
@@ -1,7 +1,7 @@
 """
 This script allows modifying the volume of active audio sessions.
 
-needed:
+Needed:
     - pycaw: Python bindings for Core Audio Windows API
     - argparse: Library for parsing command-line arguments
     - colorama: Cross-platform library for colored terminal text
@@ -9,6 +9,7 @@ needed:
 Usage:
     - View help arg
 """
+
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from pycaw.pycaw import AudioUtilities
 from colorama import init, Fore
@@ -21,12 +22,14 @@ class VolumeModifier:
     """
     Class for modifying the volume of active audio sessions.
     """
+
     def __init__(self):
         self.sessions = AudioUtilities.GetAllSessions()
 
     def list_active_sessions(self):
         """
-        Function to  list active session
+        PRE: Print the list of active sessions with their volumes.
+        POST: None
         """
         for index, session in enumerate(self.sessions):
             session_name = session.Process.name() if session.Process else "System Sounds"
@@ -36,12 +39,12 @@ class VolumeModifier:
             volume_percentage = int(session_volume * 100)
             volume_color = self.get_volume_color(session_volume)
             print(f"{index + 1}. {session_name} (Volume: "
-                f"{volume_color}{volume_percentage}%{Fore.RESET})")
-
+                  f"{volume_color}{volume_percentage}%{Fore.RESET})")
 
     def select_session(self):
         """
-        allow user to select the right session in order to modify volume value
+        PRE: Prompt the user to select a session.
+        POST: Return the selected session or None if the user chooses to quit.
         """
         while True:
             selection = input("Select a session by entering its number (or 'q' to quit): ")
@@ -54,18 +57,19 @@ class VolumeModifier:
                     session_volume = session.SimpleAudioVolume.GetMasterVolume()
                     if session_volume > 0.0:
                         return session
-                    print(f"{Fore.LIGHTRED_EX}Selected session is not currently producing sound."
+                    print(f"{Fore.LIGHTRED_EX}Selected session is not currently producing sound. "
                           f"Please choose another session.{Fore.RESET}")
                 else:
-                    print(f"{Fore.LIGHTRED_EX}Invalid selection."
-                        f"Please enter a valid number.{Fore.RESET}")
+                    print(f"{Fore.LIGHTRED_EX}Invalid selection. "
+                          f"Please enter a valid number.{Fore.RESET}")
             except ValueError:
-                print(f"{Fore.LIGHTRED_EX}Invalid selection."
-                    f"Please enter a valid number.{Fore.RESET}")
+                print(f"{Fore.LIGHTRED_EX}Invalid selection. "
+                      f"Please enter a valid number.{Fore.RESET}")
 
     def modify_volume(self, session):
         """
-        Allow to modify the volume of a specified and selected session.
+        PRE: Allow the user to enter the new volume value (0 to 100) for the selected session.
+        POST: Update the volume of the session compared to.
         """
         if session is None:
             return
@@ -80,11 +84,11 @@ class VolumeModifier:
                 new_volume = int(new_volume)
                 if 0 <= new_volume <= 100:
                     break
-                print(f"{Fore.LIGHTRED_EX}Invalid volume value."
+                print(f"{Fore.LIGHTRED_EX}Invalid volume value. "
                       f"Please enter a value between 0 and 100.{Fore.RESET}")
             except ValueError:
-                print(f"{Fore.LIGHTRED_EX}Invalid volume value."
-                    f"Please enter a valid number.{Fore.RESET}")
+                print(f"{Fore.LIGHTRED_EX}Invalid volume value. "
+                      f"Please enter a valid number.{Fore.RESET}")
 
         volume.SetMasterVolume(new_volume / 100, None)
         updated_volume = volume.GetMasterVolume()
@@ -95,17 +99,19 @@ class VolumeModifier:
     @staticmethod
     def get_volume_color(volume):
         """
-        define color of volume compared to pourcent
+        PRE: Get the volume value and determine the color based on the volume %.
+        POST: Return the color code based on the volume.
         """
         if volume <= 0.3:
-            return Fore.GREEN  # Vert
+            return Fore.GREEN
         if volume <= 0.7:
-            return Fore.YELLOW  # Jaune
-        return Fore.RED  # Rouge
+            return Fore.YELLOW
+        return Fore.RED
 
     def run(self):
         """
-        Run function
+        PRE: Run script.
+        POST: None
         """
         self.list_active_sessions()
         session = self.select_session()
@@ -118,7 +124,7 @@ if __name__ == '__main__':
         description='''
     Help description\n
     ----------------------\n
-    1. Read the list of all volume application\n
+    1. Read the list of all volume applications\n
     2. Select one of them to modify\n
     3. Change the volume\n
     ''')
